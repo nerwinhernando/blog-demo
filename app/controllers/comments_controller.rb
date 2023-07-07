8266b9dea8cb4396f2a1b4ae82e7dcbe8cb7cb1b
nerwin hernando <nerwinc.hernando@gmail.com>
2023-07-08 06:01:22 +0800

    creation of comments implementation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,33 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post
+
+  def create
+    @comments = @post.comments.create(comment_params)
+    @comments.user = current_user
+
+    if @comments.save
+      flash[:notice] = 'Comment has been created.'
+      redirect_to post_path(@post)
+    else
+      flash[:notice] = 'Comment has not been created.'
+      redirect_to post_path(@post)
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
